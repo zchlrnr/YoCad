c3966eec69d0a99f63bd17ffed124e2f8aed464f
Zach <zchlrnr@gmail.com>
2017-11-11 19:56:51 -0500

    pythonattempt02.py now has a running volume calculator and spits can calculate values for both the bearing seat subtraction and the profile shape using a basic fixed value subtraction method as well as a basic averaging trapezoidal method for the profile. It exhibits increasing accuracy as the value in Bezier.py for steps is increased

--- PythonAttempt/Bezier.py
+++ PythonAttempt/Bezier.py
@@ -12,7 +12,7 @@ def Bernstein(n, k):
     return _bpoly
 
 
-def Bezier(points, num=200):
+def Bezier(points, num=600):
     # Build Bézier curve from points.
     N = len(points)
     t = np.linspace(0, 1, num=num)
--- PythonAttempt/pythonattempt02.py
+++ PythonAttempt/pythonattempt02.py
@@ -6,6 +6,7 @@ import matplotlib.patches as patches
 from scipy.special import binom
 from Bezier import Bezier
 from BearingSubtract import BearingSubtract
+from ProfileSubtract import ProfileSubtract
 '''
     This code will be structured for the purpose of having a running
 calculation of mass.  In order to properly propogate this,
@@ -29,3 +30,17 @@ Blank_Volume = ((Blank_Radius**2)*math.pi)*Blank_Length
 RunningVolume = Blank_Volume
 # THIS IS THE SUBTRACTION OF THE CUT FROM THE BEARING
 RunningVolume = RunningVolume -  BearingSubtract(3)
+# Now, The volume of the profile needs to be subtracted
+# Pxc and Pyc are the coordinates of the control points in from
+# ... 'profile.txt'
+Pxc = []
+Pyc = []
+Pdata = np.loadtxt('profile.txt', delimiter=',')
+for i in range(0,len(Pdata)):
+    Pxc = np.append(Pxc, Pdata[i,0])
+    Pyc = np.append(Pyc, Pdata[i,1])
+# Px and Py are the many value long column vectors of the data points
+# ... which compose the Bezier curve
+Px, Py = Bezier(list(zip(Pxc, Pyc))).T
+RunningVolume = RunningVolume - ProfileSubtract(Px, Py, RunningVolume)
+print(RunningVolume)

a79d8d83946bdd5fb3da33d959985f490874377d
Zach <zchlrnr@gmail.com>
2017-11-14 23:15:58 -0500

    Got threedgen succesfully making craptons of points in roation about x axis.  the downside is that it eats 4 gigs of ram and doesn't look right at all. Still need to figure out how to make surfaces work.  End me.

--- README.md
+++ README.md
@@ -42,6 +42,7 @@
     $pip3 install numpy
     $pip3 install scipy
     $pip3 install matplotlib
+    $pip3 install dxfwrite
     $python3 pythonattempt02.py
 ```
 
--- ReleaseBuild_01/threedgen.py
+++ ReleaseBuild_01/threedgen.py
@@ -1,19 +1,35 @@
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
+import math
+from math import pi
 def threedgen(bulkmat, angsteps):
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    a = bulkmat.tolist()
-    print(a)
-    #for i in range(0, len(bulkmat)):
+    x=[]
+    y=[]
+    z=[]
+    bigx=[]
+    bigy=[]
+    bigz=[]
+    for i in range(0, len(bulkmat)):
+        x.append(bulkmat[i,0])
+        y.append(bulkmat[i,1])
+        z.append(0)
+    for i in range(0,len(bulkmat)):
+        for j in range(0,angsteps):
+            # j is current angular step number
+            # i is current point in original profile
+            # the y value of the i'th entry is the radius
+            bigx.append(x[i])
+            bigy.append(math.cos(j*(2*pi)/angsteps)*y[i])
+            bigz.append(math.sin(j*(2*pi)/angsteps)*y[i])
 
+    ax.scatter(bigx, bigy, bigz)
 
-    #ax.scatter(xs, ys, zs)
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
 
-    #ax.set_xlabel('X Label')
-    #ax.set_ylabel('Y Label')
-    #ax.set_zlabel('Z Label')
-
-    #plt.show()
+    plt.show()

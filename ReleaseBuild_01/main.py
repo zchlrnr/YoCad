637a8010bd86585bb11a392cb2ac31c5c54d63a5
Zach <zchlrnr@gmail.com>
2017-11-23 10:07:51 -0500

    THE PLOTTING AND CONCATENATION IS NOW HANDLED BY 'Bulkmat.py'

--- ReleaseBuild_01/main.py
+++ ReleaseBuild_01/main.py
@@ -60,7 +60,7 @@ megamaty = np.concatenate((Py, Ry, Cy),axis=0)
 bulkmat = np.concatenate((Bdata,megamat), axis=0)
 '''
 # This is the new version of the code above
-megamatx, megamaty, prof, rim, cup, bulkmat = Bulkmat(Px, Py, Rx, Ry, Cx, Cy, Bdata)
+megamatx, megamaty, prof, rim, cup, bulkmat = Bulkmat(Px, Py, Rx, Ry, Cx, Cy, Bdata, Pdata, Rdata, Cdata)
 print(bulkmat)
 #Creates coordinate pair that makes up physical location of centroid of
 #... 2d quarter section, and computes RimWeightRatio
@@ -81,35 +81,34 @@ if floatingminthick <= thick_criteria:
     farea = fracturearea(floatingminthick, thick_criteria, Sdata, prof, rim, cup)
 # OUTPUT SECTION
 # Configuration of the text in the 2d image
-font = {'family': 'sans',
-        'color':  'black',
-        'weight': 'normal',
-        'size': 16,
-        }
-plt.title('Scale Quarter Section (mm)', fontdict=font)
-plt.xlabel('x axis (mm)',fontdict=font)
-plt.ylabel('y axis (mm)',fontdict=font)
-mass = str(halfmass)[:6]
-plt.text(15, 5, 'mass =%s grams' % (mass))
-Cxs = str(Cx)[:5]
-Cys = str(Cy)[:5]
-plt.text(12,3.5, 'Center of mass @ (%s, %s)' %(Cxs, Cys))
-RimWeight_s = str(RimWeightRatio)[:6]
-plt.text(13, 2, 'RimWeightRatio = %s' % RimWeight_s)
+#font = {'family': 'sans',
+#        'color':  'black',
+#        'weight': 'normal',
+#        'size': 16,
+#        }
+#plt.title('Scale Quarter Section (mm)', fontdict=font)
+#plt.xlabel('x axis (mm)',fontdict=font)
+#plt.ylabel('y axis (mm)',fontdict=font)
+#mass = str(halfmass)[:6]
+#plt.text(15, 5, 'mass =%s grams' % (mass))
+#Cxs = str(Cx)[:5]
+#Cys = str(Cy)[:5]
+#plt.text(12,3.5, 'Center of mass @ (%s, %s)' %(Cxs, Cys))
+#RimWeight_s = str(RimWeightRatio)[:6]
+#plt.text(13, 2, 'RimWeightRatio = %s' % RimWeight_s)
 #THIS ONE LINE BELOW IS RESPONSIBLE FOR PLOTTING EVERYTHING THAT I
 #... NOT THE BEARING SEAT
-plt.plot(megamatx, megamaty, 'k')
+#plt.plot(megamatx, megamaty, 'k')
 #This plots the bearing seat, WITHOUT AXLE HOLE YET DRILLED .
-plt.plot(Bdata[:,0], Bdata[:,1], 'k')
+#plt.plot(Bdata[:,0], Bdata[:,1], 'k')
 #This plots the section of filled area if and only if the minimum wall
 #... thickness criteria is violated.
-if thickflag == 1:
-    plt.fill(farea[:,0],farea[:,1], 'k', alpha=0.3)
-plt.plot()
-plt.axis('equal')
-plt.savefig('figure.png')
-plt.grid()
-plt.show()
+#if thickflag == 1:
+#    plt.fill(farea[:,0],farea[:,1], 'k', alpha=0.3)
+#plt.axis('equal')
+#plt.savefig('figure.png')
+#plt.grid()
+#plt.show()
 #Generation of 3d plot in matplotlib
 angsteps = 20
 threedgen(bulkmat, angsteps)
